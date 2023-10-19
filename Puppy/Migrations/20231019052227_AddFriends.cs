f82dc82830dad4d5df3ad772ea1f695dc35ab6ea
tedecti <wolftheguy08@gmail.com>
2023-10-19 11:23:28 +0600

    xz

---
+++ Puppy/Migrations/20231019052227_AddFriends.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Puppy.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFriends : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
