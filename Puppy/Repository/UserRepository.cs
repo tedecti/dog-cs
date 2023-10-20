6b930ef08789bd5386acc878244795ed21e24981
Agaspher <agasphergg@gmail.com>
2023-10-20 10:37:09 +0600

    password hashing

--- Puppy/Repository/UserRepository.cs
+++ Puppy/Repository/UserRepository.cs
@@ -9,77 +9,84 @@ using System.Security.Claims;
 
 namespace Puppy.Repository
 {
-	public class UserRepository : IUserRepository
-	{
-		private readonly AppDbContext _context;
-		private string secretKey;
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _context;
+        private string secretKey;
 
-		public UserRepository(AppDbContext context, IConfiguration configuration)
-		{
-			_context = context;
-			secretKey = configuration.GetValue<string>("ApiSettings:Secret");
-		}
+        public UserRepository(AppDbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            secretKey = configuration.GetValue<string>("ApiSettings:Secret");
+        }
 
-		public bool IsUniqueUser(string email)
-		{
-			var user = _context.Users.FirstOrDefault(x => x.Email == email);
-			if (user == null)
-			{
-				return true;
-			}
-			return false;
-		}
+        public bool IsUniqueUser(string email)
+        {
+            var user = _context.Users.FirstOrDefault(x => x.Email == email);
+            if (user == null)
+            {
+                return true;
+            }
 
-		public async Task<LoginResponseDto> Login(LoginRequestDto loginRequestDTO)
-		{
-			var user = _context.Users.FirstOrDefault(u => u.Email == loginRequestDTO.Email && u.Password == loginRequestDTO.Password);
+            return false;
+        }
 
-			if (user == null)
-			{
-				return new LoginResponseDto()
-				{
-					User = null,
-					Token = ""
-				}; ;
-			}
+        public async Task<LoginResponseDto> Login(LoginRequestDto loginRequestDTO)
+        {
+            var user = _context.Users.FirstOrDefault(u =>
+                u.Email == loginRequestDTO.Email );
 
-			var tokenHandler = new JwtSecurityTokenHandler();
-			var key = Encoding.ASCII.GetBytes(secretKey);
+            bool isVerified = BCrypt.Net.BCrypt.Verify(loginRequestDTO.Password, user.Password);
+            
+            if (user == null || !isVerified)
+            {
+                return new LoginResponseDto()
+                {
+                    User = null,
+                    Token = ""
+                };
+                ;
+            }
 
-			var tokenDescriptor = new SecurityTokenDescriptor
-			{
-				Subject = new ClaimsIdentity(new Claim[]
-				{
-					new Claim(ClaimTypes.Name, user.Id.ToString()),
-					new Claim(ClaimTypes.Role, "User"),
-				}),
-				Expires = DateTime.UtcNow.AddDays(7),
-				SigningCredentials = new(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
-			};
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secretKey);
 
-			var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString()),
+                    new Claim(ClaimTypes.Role, "User"),
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+            };
 
-			LoginResponseDto loginResponseDTO = new LoginResponseDto()
-			{
-				User = user,
-				Token = tokenHandler.WriteToken(token)
-			};
-			return loginResponseDTO;
-		}
+            var token = tokenHandler.CreateToken(tokenDescriptor);
 
-		public async Task<User> Register(RegistrationRequestDto registerRequestDTO)
-		{
-			User user = new User()
-			{
-				Email = registerRequestDTO.Email,
-				FirstName = registerRequestDTO.FirstName,
-				LastName = registerRequestDTO.LastName,
-				Password = registerRequestDTO.Password,
-			};
-			_context.Users.Add(user);
-			await _context.SaveChangesAsync();
-			user.Password = "";
-			return user;
-		}
-	}
+            LoginResponseDto loginResponseDTO = new LoginResponseDto()
+            {
+                User = user,
+                Token = tokenHandler.WriteToken(token)
+            };
+            return loginResponseDTO;
+        }
+
+        public async Task<User> Register(RegistrationRequestDto registerRequestDTO)
+        {
+            string password = BCrypt.Net.BCrypt.HashPassword(registerRequestDTO.Password, 12);
+
+            User user = new User()
+            {
+                Email = registerRequestDTO.Email,
+                FirstName = registerRequestDTO.FirstName,
+                LastName = registerRequestDTO.LastName,
+                Password = password,
+            };
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            user.Password = "";
+            return user;
+        }
+    }
 }
