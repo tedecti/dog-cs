974d5523eefbc82d3fd1c9ea84e00f7da0567eba
tedecti <wolftheguy08@gmail.com>
2023-11-25 13:18:27 +0600

    i tried

--- Puppy/Data/AppDbContext.cs
+++ Puppy/Data/AppDbContext.cs
@@ -5,8 +5,22 @@ namespace Curs.Data
 {
     public class AppDbContext : DbContext
     {
+        private static string Get()
+        {
+            var uri = new Uri(
+                "postgres://qtzdvcyq:jXT76DhO3geh4fq7halb_gCFVayQjShJ@cornelius.db.elephantsql.com/qtzdvcyq");
+            var db = uri.AbsolutePath.Trim('/');
+            var user = uri.UserInfo.Split(':')[0];
+            var passwd = uri.UserInfo.Split(':')[1];
+            var port = uri.Port > 0 ? uri.Port : 5432;
+            var connStr = string.Format("Server={0};Database={1};User Id={2};Password={3};Port={4}",
+                uri.Host, db, user, passwd, port);
+            Console.WriteLine(connStr);
+            return connStr;
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-            => optionsBuilder.UseNpgsql("Host=127.0.0.1;Database=kurs;Username=postgres;Password=123");
+            => optionsBuilder.UseNpgsql(Get());
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
