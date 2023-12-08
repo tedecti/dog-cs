b22a078c890d878ada07f76cbe609badba62810b
tedecti <wolftheguy08@gmail.com>
2023-12-08 20:25:22 +0600

    IUserRepository change

--- Puppy/Repository/IRepository/IUserRepository.cs
+++ Puppy/Repository/IRepository/IUserRepository.cs
@@ -5,10 +5,7 @@ namespace Puppy.Repository.IRepository
 {
 	public interface IUserRepository
 	{
-		bool IsUniqueEmail(string email);
-		bool IsUnique (string email, string username);
 		Task<LoginResponseDto> Login(LoginRequestDto loginRequestDTO);
 		Task<User> Register(RegistrationRequestDto registerRequestDTO);
-		Task<User> GetUser(int userId);
 	}
 }
