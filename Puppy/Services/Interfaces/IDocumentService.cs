49c4ec8d0994c0feeef622a6477b97a254ccb3f3
tedecti <wolftheguy08@gmail.com>
2023-12-13 18:40:42 +0600

    change some things

---
+++ Puppy/Services/Interfaces/IDocumentService.cs
@@ -0,0 +1,10 @@
+using Curs.Models;
+
+namespace Puppy.Services.Interfaces;
+
+public interface IDocumentService
+{
+    Task<IEnumerable<Document>> GetDocumentsByPet(int petId);
+    Task<Document> GetDocumentById(int documentId);
+    
+}
