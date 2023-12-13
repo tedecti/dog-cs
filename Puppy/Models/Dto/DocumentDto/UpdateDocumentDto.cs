e4b72be03025b36fa471f66ea4ad61eb9527241b
tedecti <wolftheguy08@gmail.com>
2023-12-13 20:30:31 +0600

    UpdateDocumentDto

---
+++ Puppy/Models/Dto/DocumentDto/UpdateDocumentDto.cs
@@ -0,0 +1,9 @@
+namespace Puppy.Models.Dto.DocumentDto;
+
+public class UpdateDocumentDto
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    
+    public List<IFormFile> Imgs { get; set; }
+}
