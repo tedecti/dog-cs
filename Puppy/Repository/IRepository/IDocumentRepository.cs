e2004d152f178a4d7ad77e4f816a0b1c9538c91f
tedecti <wolftheguy08@gmail.com>
2023-12-13 20:29:59 +0600

    DR Interface

--- Puppy/Repository/IRepository/IDocumentRepository.cs
+++ Puppy/Repository/IRepository/IDocumentRepository.cs
@@ -1,11 +1,12 @@
 using Curs.Models;
 using Curs.Models.Dto.DocumentDto;
+using Puppy.Models.Dto.DocumentDto;
 
 namespace Puppy.Repository.IRepository;
 
 public interface IDocumentRepository
 {
-    Task<Document> CreateDocument(UploadDocumentDto uploadDocumentDto);
-    Task<Document> EditDocument();
+    Task<Document> CreateDocument(UploadDocumentDto uploadDocumentDto, int petId);
+    Task<Document> EditDocument(UpdateDocumentDto updateDocumentDto, int documentId);
     Task<Document> DeleteDocument(int documentId);
 }
