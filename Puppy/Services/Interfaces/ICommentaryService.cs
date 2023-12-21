fdfa8e8c3b34592c8884aacc95e94bcb5fa6bf66
tedecti <wolftheguy08@gmail.com>
2023-12-21 22:34:40 +0600

    + GetComment

--- Puppy/Services/Interfaces/ICommentaryService.cs
+++ Puppy/Services/Interfaces/ICommentaryService.cs
@@ -6,4 +6,5 @@ public interface ICommentaryService
 {
     Task<IEnumerable<Commentary>> GetCommentaries(int postId);
     Task<int> GetTotal(int postId);
+    Task<Commentary> GetComment(int commentId);
 }
