b3104ea720efeff177350372cb510b411c8649db
tedecti <wolftheguy08@gmail.com>
2023-12-21 22:34:54 +0600

    CommentRepo

---
+++ Puppy/Repository/IRepository/ICommentaryRepository.cs
@@ -0,0 +1,10 @@
+using Curs.Models;
+using Puppy.Models.Dto;
+
+namespace Puppy.Repository.IRepository;
+
+public interface ICommentaryRepository
+{
+    Task<Commentary> CreateCommentary(AddCommentaryRequestDto addCommentaryRequestDto, int userId, int postId);
+    Task<Commentary> EditCommentary(AddCommentaryRequestDto addCommentaryRequestDto, int userId, int commentaryId);
+}
