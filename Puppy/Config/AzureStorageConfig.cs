92ebea9146414f4f143ac0d3c40613483d7df2f2
tedecti <wolftheguy08@gmail.com>
2024-04-20 20:19:13 +0500

    appsettings fix

--- Puppy/Config/AzureStorageConfig.cs
+++
@@ -1,9 +0,0 @@
-namespace Puppy.Config;
-
-public class AzureStorageConfig
-{
-    public string AccountName { get; set; }
-    public string AccountKey { get; set; }
-    public string ImageContainer { get; set; }
-    public string ConnectionString { get; set; }
-}
