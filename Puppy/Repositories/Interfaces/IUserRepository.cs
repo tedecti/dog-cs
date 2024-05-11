c0920c1b3297efa0592c9363db9022b682bd9a4b
tedecti <wolftheguy08@gmail.com>
2024-05-11 20:50:32 +0500

    forgor

---
+++ Puppy/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using Puppy.Models;
+using Puppy.Models.Dto.AuthDtos;
+using Puppy.Models.Dto.UserDtos;
+
+namespace Puppy.Repositories.Interfaces
+{
+	public interface IUserRepository
+	{
+		Task<LoginResponseDto?> Login(LoginRequestDto loginRequestDto);
+		Task<User> Register(RegistrationRequestDto registerRequestDto);
+		Task<User?> Edit(UpdateUserDto updateUserDto, int userId);
+		Task<User> UploadAvatar(int userId, string fName);
+		Task<IEnumerable<User>> GetUsers();
+		bool IsUniqueEmail(string email);
+		bool IsUnique (string email, string username);
+		Task<User> GetUser(int userId);
+	}
+}
