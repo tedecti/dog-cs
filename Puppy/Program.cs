e4dfb83164f4749cd65da381990e682f8f28a7cf
tedecti <wolftheguy08@gmail.com>
2024-05-22 20:13:35 +0500

    FileUploading

--- Puppy/Program.cs
+++ Puppy/Program.cs
@@ -67,7 +67,7 @@ namespace Puppy
             builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen(setup =>
             {
@@ -80,16 +80,16 @@ namespace Puppy
                     Type = SecuritySchemeType.Http,
                     Scheme = JwtBearerDefaults.AuthenticationScheme,
                     Description = "Put **_ONLY_** your JWT Bearer token on textbox below!",
-
+            
                     Reference = new OpenApiReference
                     {
                         Id = JwtBearerDefaults.AuthenticationScheme,
                         Type = ReferenceType.SecurityScheme
                     }
                 };
-
+            
                 setup.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
-
+            
                 setup.AddSecurityRequirement(new OpenApiSecurityRequirement
                 {
                     { jwtSecurityScheme, Array.Empty<string>() }
@@ -108,7 +108,7 @@ namespace Puppy
 
             app.UseAuthentication();
             app.UseAuthorization();
-
+            
 
             app.MapControllers();
             app.UseCors("MyPolicy");
