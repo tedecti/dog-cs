e7e89c1366d9957a12ee612e337b9cfddbae07cc
tedecti <wolftheguy08@gmail.com>
2024-06-02 17:46:54 +0500

    cors fix 2

--- Puppy/Program.cs
+++ Puppy/Program.cs
@@ -39,7 +39,6 @@ namespace Puppy
             {
                 builder.AllowAnyOrigin()
                     .AllowAnyMethod()
-                    .AllowCredentials()
                     .AllowAnyHeader();
             }));
 
